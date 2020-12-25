469321cade10d21703d75e62c8ce14d4128998b0
waihon <yewwaihon@gmail.com>
2020-12-25 08:14:55 +0000

    Forward messages to BottleNumber

--- bottles.rb
+++ bottles.rb
@@ -19,35 +19,19 @@ class Bottles
   end
 
   def container(number)
-    if number == 1
-      "bottle"
-    else
-      "bottles"
-    end
+    BottleNumber.new(number).container(number)
   end
 
   def action(number)
-    if number == 0
-      "Go to the store and buy some more"
-    else
-      "Take #{pronoun(number)} down and pass it around"
-    end
+    BottleNumber.new(number).action(number)
   end
 
   def pronoun(number)
-    if number == 1
-      "it"
-    else
-      "one"
-    end
+    BottleNumber.new(number).pronoun(number)
   end
 
   def successor(number)
-    if number == 0
-      99
-    else
-      number - 1
-    end
+    BottleNumber.new(number).successor(number)
   end
 end
 
