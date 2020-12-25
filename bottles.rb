442f70a7bca924713acc4051e99693a458979be3
waihon <yewwaihon@gmail.com>
2020-12-25 03:20:36 +0000

    Rearrange sequence of methods

--- bottles.rb
+++ bottles.rb
@@ -1,33 +1,17 @@
 class Bottles
-  def verse(number)
-    "#{quantity(number).capitalize} #{container(number)} of milk on the wall, " +
-    "#{quantity(number)} #{container(number)} of milk.\n" +
-    "#{action(number)}, " +
-    "#{quantity(successor(number))} #{container(successor(number))} of milk on the wall.\n"
+  def song
+    verses(99, 0)
   end
 
   def verses(upper, lower)
     upper.downto(lower).map { |i| verse(i) }.join("\n") 
   end
 
-  def song
-    verses(99, 0)
-  end
-
-  def container(number)
-    if number == 1
-      "bottle"
-    else
-      "bottles"
-    end
-  end
-
-  def pronoun(number)
-    if number == 1
-      "it"
-    else
-      "one"
-    end
+  def verse(number)
+    "#{quantity(number).capitalize} #{container(number)} of milk on the wall, " +
+    "#{quantity(number)} #{container(number)} of milk.\n" +
+    "#{action(number)}, " +
+    "#{quantity(successor(number))} #{container(successor(number))} of milk on the wall.\n"
   end
 
   def quantity(number)
@@ -38,11 +22,27 @@ class Bottles
     end
   end
 
+  def container(number)
+    if number == 1
+      "bottle"
+    else
+      "bottles"
+    end
+  end
+
   def action(number)
     if number == 0
       "Go to the store and buy some more"
     else
       "Take #{pronoun(number)} down and pass it around"
+    end
+  end
+
+  def pronoun(number)
+    if number == 1
+      "it"
+    else
+      "one"
     end
   end
 
