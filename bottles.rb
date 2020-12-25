064e162dcf02d996c10237c08db22d548c131ae1
waihon <yewwaihon@gmail.com>
2020-12-25 08:38:11 +0000

    Forward container without redundancy

--- bottles.rb
+++ bottles.rb
@@ -19,7 +19,7 @@ class Bottles
   end
 
   def container(number)
-    BottleNumber.new(number).container(number)
+    BottleNumber.new(number).container
   end
 
   def action(number)
@@ -50,7 +50,7 @@ class BottleNumber
     end
   end
 
-  def container(number)
+  def container
     if number == 1
       "bottle"
     else
