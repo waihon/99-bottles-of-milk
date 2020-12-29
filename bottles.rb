179a7d2c7a288c9e616eb8e512c13eccedc002f7
Waihon Yew <yewwaihon@gmail.com>
2020-12-29 15:22:12 +0000

    Use the verse template

--- bottles.rb
+++ bottles.rb
@@ -4,7 +4,7 @@ class Bottles
   def initialize(verse_template: BottleVerse)
     @verse_template = verse_template
   end
-  
+
   def song
     verses(99, 0)
   end
@@ -14,8 +14,7 @@ class Bottles
   end
 
   def verse(number)
-    # verse_template.new(number).lyrics
-    BottleVerse.new(number).lyrics
+    verse_template.new(number).lyrics
   end
 end
 
