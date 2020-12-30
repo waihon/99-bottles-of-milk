92d29205f3a34f63532a47aec60f8dde032d3854
Waihon Yew <yewwaihon@gmail.com>
2020-12-30 02:33:54 +0000

    Set bottle number to number

--- bottles.rb
+++ bottles.rb
@@ -125,10 +125,11 @@ class BottleVerse
     new(BottleNumber.for(number)).lyrics
   end
 
-  attr_reader :number
+  attr_reader :number, :bottle_number
 
   def initialize(number)
     @number = number
+    @bottle_number = @number
   end
 
   def lyrics
