b1117f9bb8af6047cfbef04e526822087c7d5a88
Waihon Yew <yewwaihon@gmail.com>
2020-12-31 14:29:41 +0000

    Test to verify verse role player

--- bottles_test.rb
+++ bottles_test.rb
@@ -3,6 +3,12 @@
 require 'minitest/autorun'
 require_relative 'bottles'
 
+module VerseRoleTest
+  def test_plays_verse_role
+    assert_respond_to @role_player, :lyrics
+  end
+end
+
 class CountdownSongTest < Minitest::Test
   def test_verse
     expected = "This is verse 500.\n"
