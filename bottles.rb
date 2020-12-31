42a28a330656340080b6238db35db52b62ab7417
Waihon Yew <yewwaihon@gmail.com>
2020-12-31 06:17:28 +0000

    Duplicate Bottles as CountdownSong

--- bottles.rb
+++ bottles.rb
@@ -18,6 +18,26 @@ class Bottles
   end
 end
 
+class CountdownSong
+  attr_reader :verse_template
+
+  def initialize(verse_template: BottleVerse)
+    @verse_template = verse_template
+  end
+
+  def song
+    verses(99, 0)
+  end
+
+  def verses(upper, lower)
+    upper.downto(lower).map { |i| verse(i) }.join("\n") 
+  end
+
+  def verse(number)
+    verse_template.lyrics(number)
+  end
+end
+
 class BottleNumber
   def self.for(number)
     registry.find do |candidate|
