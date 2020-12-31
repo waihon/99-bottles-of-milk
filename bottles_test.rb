0ea0ebf4a1897f918e6d8059eaf7ac1b49508bd7
Waihon Yew <yewwaihon@gmail.com>
2020-12-31 13:20:43 +0000

    Test entire API in CountdownSongTest

--- bottles_test.rb
+++ bottles_test.rb
@@ -4,6 +4,14 @@ require 'minitest/autorun'
 require_relative 'bottles'
 
 class CountdownSongTest < Minitest::Test
+  def test_verse
+    expected = "This is verse 500.\n"
+    assert_equal(
+      expected,
+      CountdownSong.new(verse_template: VerseFake)
+        .verse(500))
+  end
+
   def test_a_couple_verses
     expected =
       "This is verse 99.\n" +
